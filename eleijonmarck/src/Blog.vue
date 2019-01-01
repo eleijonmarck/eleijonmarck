e082c10506a35041ef47266f7ac011676369b8ca
eleijonmarck <eric.leijonmarck@gmail.com>
2019-01-01 19:43:14 +0100

    searchable blogggs mfs

--- eleijonmarck/src/Blog.vue
+++ eleijonmarck/src/Blog.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="col-sm-12">
+  <input class="form-control input-sm" type="text" v-model="search" placeholder="search-here">
         <paginate
           name="blogs"
           :list="posts"
@@ -7,7 +8,7 @@
           tag="div"
         >
         
-        <section v-for="blog in paginated('blogs')">
+        <section v-for="blog in filteredBlogs">
           <h2>{{ blog.title }}</h2>
           <router-link :to="'/post/' + blog.id" class="btn btn-primary">read more</router-link>
           <hr>
@@ -38,13 +39,21 @@ export default {
   data () {
     return {
       posts: [],
-      paginate: ['blogs']
+      paginate: ['blogs'],
+      search: ""
     }
   },
   created(){
     this.$http.get("http://jsonplaceholder.typicode.com/posts")
       .then(response => response.json(), error => console.log(error))
       .then(json => this.posts = json, error => console.log(error));
+  },
+  computed: {
+    filteredBlogs: function(){
+      return this.paginated('blogs').filter((blog) => {
+        return blog.title.match(this.search);
+      });
+    }
   }
 }
 </script>
