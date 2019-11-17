a306aaab8518aa5af405b108bfbc8d7dc23ba97b
eleijonmarck <eric.leijonmarck@gmail.com>
2019-11-17 20:38:06 +0100

    updated title

--- gridsome.config.js
+++ gridsome.config.js
@@ -5,7 +5,7 @@
 // To restart press CTRL + C in terminal and run `gridsome develop`
 
 module.exports = {
-  siteName: '#TODO: A*.find(information)',
+  siteName: 'a_star.find(information)',
   siteDescription: 'A way to find some information',
 
   templates: {
