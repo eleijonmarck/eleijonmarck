afd452f920ecd5e664c63df34c12317fa86180ca
eleijonmarck <eric.leijonmarck@gmail.com>
2020-01-10 17:56:44 +0100

    made image of me appear

--- config.js
+++ config.js
@@ -3,13 +3,13 @@
 module.exports = {
   url: 'https://lumen.netlify.com',
   pathPrefix: '/',
-  title: 'Blog by John Doe',
+  title: 'Blog by Eric Leijonmarck',
   subtitle: 'Pellentesque odio nisi, euismod in, pharetra a, ultricies in, diam. Sed arcu.',
   copyright: '© All rights reserved.',
   disqusShortname: '',
   postsPerPage: 4,
   googleAnalyticsId: 'UA-73379983-2',
-  useKatex: false,
+  useKatex: true,
   menu: [
     {
       label: 'Articles',
@@ -25,19 +25,19 @@ module.exports = {
     }
   ],
   author: {
-    name: 'John Doe',
-    photo: '/photo.jpg',
+    name: 'Eric Leijonmarck',
+    photo: '/eric-small.png',
     bio: 'Pellentesque odio nisi, euismod in, pharetra a, ultricies in, diam. Sed arcu.',
     contacts: {
       email: '',
-      facebook: '#',
-      telegram: '#',
-      twitter: '#',
-      github: '#',
+      facebook: '',
+      telegram: '',
+      twitter: 'eleijonmarck',
+      github: 'eleijonmarck',
       rss: '',
       vkontakte: '',
-      linkedin: '#',
-      instagram: '#',
+      linkedin: 'eleijonmarck',
+      instagram: '',
       line: '',
       gitlab: '',
       weibo: '',
