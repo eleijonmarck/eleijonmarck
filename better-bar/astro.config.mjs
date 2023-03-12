e4da532a8630bece8f4849b59d069cddf7a7ef7c
Eric Leijonmarck <eric.leijonmarck@gmail.com>
2023-03-12 09:35:20 +0000

    deploy test

--- better-bar/astro.config.mjs
+++ better-bar/astro.config.mjs
@@ -2,6 +2,8 @@ import { defineConfig } from 'astro/config';
 import svelte from '@astrojs/svelte';
 import mdx from '@astrojs/mdx';
 
+import netlify from '@astrojs/netlify/functions';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://astro-blog-template.netlify.app',
@@ -14,4 +16,6 @@ export default defineConfig({
       target: '_blank'
     }]]
   },
+  output: "server",
+  adapter: netlify(),
 });
